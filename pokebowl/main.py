137211384ee560ab70853280f33f5c1664b3c11a
Noyx46 <61034921+Noyx46@users.noreply.github.com>
2022-07-03 16:37:00 -0400

    feat: make a moving rectangle

--- pokebowl/main.py
+++ pokebowl/main.py
@@ -1,7 +1,58 @@
+from typing import Sequence
+
 import pygame
+from pygame.locals import K_DOWN, K_ESCAPE, K_LEFT, K_RIGHT, K_UP, KEYDOWN, QUIT
+
+# constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((100, 100))
+        self.surf.fill((255, 50, 30))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys: Sequence[bool]):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
 
 if __name__ == "__main__":
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+    clock = pygame.time.Clock()
+
+    player = Player()
+
+    running = True
+    while running:
+        # Process events
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+            if event.type == QUIT:
+                running = False
+
+        pressed_keys = pygame.key.get_pressed()
+        player.update(pressed_keys)
+
+        screen.fill((255, 255, 255))
+        # Put player on screen
+        screen.blit(player.surf, player.rect)
+        # Display stuff
+        pygame.display.flip()
+
+        # Maintain frame rate
+        clock.tick(60)
+    pygame.quit()
